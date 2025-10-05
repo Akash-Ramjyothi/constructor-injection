d79c2cf1153a4c65aaffa2fa68c25003ac42efd9
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-10-05 23:17:07 +0530

    Added Bean lifecycle methods

--- src/main/java/com/constructor/injection/common/CricketCoach.java
+++ src/main/java/com/constructor/injection/common/CricketCoach.java
@@ -1,9 +1,21 @@
 package com.constructor.injection.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("▶️ Do my Startup Stuff: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("🧹 Do my Cleanup Stuff: " + getClass().getSimpleName());
+    }
 
     public CricketCoach() {
         System.out.println("🏏 In constructor: " + getClass().getSimpleName());
--- src/main/java/com/constructor/injection/rest/InjectionController.java
+++ src/main/java/com/constructor/injection/rest/InjectionController.java
@@ -10,23 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class InjectionController {
 
     private Coach myCoach;
-    private Coach anotherCoach;
 
     @Autowired
-    public InjectionController(@Qualifier("cricketCoach") Coach theCoach, @Qualifier("cricketCoach") Coach theAnotherCoach) {
+    public InjectionController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("💉In Constructor: " + getClass().getSimpleName());
 
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return ("🫘 Comparing Beans myCoach == anotherCoach : " + (myCoach == anotherCoach));
     }
 }
